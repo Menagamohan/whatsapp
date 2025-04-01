f7824b4d6145ccc44d591cbb0dc5470278c8e0b2
Menaga Mohanraj <menagamohan26@gmail.com>
2025-04-01 17:39:31 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,123 @@
+<!--footer start-->
+
+<footer id="footer" class="footer white-bg z-index-1 overflow-hidden bg-contain" data-bg-img="images/pattern/01.png">
+        <div class="round-p-animation"></div>
+        <div class="primary-footer">
+            <div class="container-fluid p-0">
+                <div class="row">
+                    <div class="col-lg-4" style="background-color: #2cb0fd;">
+                        <div class="ht-theme-info bg-contain bg-pos-r h-100 dark-bg text-white" data-bg-img="images/bg/02.png">
+                            <div class="footer-logo">
+                                <a href="index.html">
+                                    <img class="img-fluid" src="images/logo1.jpg" alt="askeva-logo">
+                                </a>
+                            </div>
+                            <p class="mb-3" style="text-align: justify; font-size: 20px;">LeMeniz is powered through the Official WhatsApp Business API and easily aligned with WhatsApp rules.WhatsApp Broadcast will provide a green tick badge that helps you in sending rich multimedia messages and sample messaging
+                                option that develop your business. All in one Whatsapp based chatbot for business.</p><br>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="social-icons social-border circle social-hover ">
+                                        <h4 class="title" style="margin-bottom: 1px;">Follow Us</h4>
+                                        <div class="gradient-floor blue-to-purple"></div><br>
+                                        <div class="container">
+                                            <div class="row row-cols-auto">
+                                                <div class="col">
+                                                    <a href="https://www.facebook.com/askeva.io" target="_blank"><i class="fab fa-facebook-f" style="font-size: 30px; color: #f6f9fa;"></i></a>
+                                                </div>
+                                                <div class="col">
+                                                    <a href="https://www.instagram.com/askeva.io/" target="_blank"><i class="fab fa-instagram" style="font-size: 30px; color: #f6f9fa;"></i></a>
+                                                </div>
+                                                <div class="col">
+                                                    <a href="https://www.linkedin.com/company/askevaio/" target="_blank"><i class="fab fa-linkedin-in" style="font-size: 30px; color: #f6f9fa;"></i></a>
+                                                </div>
+                                                <div class="col">
+                                                    <a href="https://www.youtube.com/channel/UCKBwYFFB2GbSnXhhowRk7qQ" target="_blank"><i class="fab fa-youtube" style="font-size: 30px; color: #f6f9fa;"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-8 py-8 px-5" style="padding-bottom: 30px !important;">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-2 col-lg-4 footer-list">
+                                <h4 class="title">Company</h4>
+                                <div class="gradient-floor blue-to-purple"></div><br>
+                                <ul class="list-unstyled">
+                                    <li><a href="terms-condition.html">Terms & Condition</a></li>
+                                    <li><a href="refund.html">Refund policy</a></li>
+                                    <li><a href="privacypolicy.html">Privacy Policy</a></li>
+                                </ul><br>
+                                <h4 class="title">Payment</h4>
+                                <div class="gradient-floor blue-to-purple"></div><br>
+                                <ul class="list-unstyled">
+                                    <li><a href="https://rzp.io/l/MqwwHFmuep" target="_blank">Pay Online</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-4 mt-5 mt-md-0 footer-list">
+                                <h4 class="title">Solution</h4>
+                                <div class="gradient-floor blue-to-purple"></div><br>
+                                <ul class="media-icon list-unstyled">
+                                    <li><a href="chatbot.html">Whatsapp API Chatbot</a></li>
+                                    <li><a href="whatsappautomation.html">Whatsapp Automation</a></li>
+                                    <li><a href="whatsappecommerce.html">Whatsapp E-Commerce</a></li>
+                                    <li><a href="customersupport.html">Whatsapp Customer Support</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-12 col-md-4 col-lg-4 mt-5 mt-md-0 footer-list">
+                                <h4 class="title">Resources</h4>
+                                <div class="gradient-floor blue-to-purple"></div><br>
+                                <ul class="media-icon list-unstyled">
+                                    <li><a href="education.html">Case Study</a></li>
+                                    <li><a href="https://askeva.gitbook.io/askeva-docs/faqs/faqs" target="_blank">FAQs</a></li>
+                                    <li><a href="career.html">Career</a></li>
+                                    <li><a href="https://askeva.gitbook.io/askeva-docs/">Help Desk</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="row " style="background-color: #f6f9fa;">
+                            <div class="col-lg-11 col-md-12 me-auto">
+                                <div class="align-items-center white-bg box-shadow px-3 py-3 radius d-md-flex justify-content-between">
+                                    <h5 class="mb-0">Get a Free Whatsapp API</h5>
+                                    <div class="subscribe-form sm-mt-2">
+                                        <form id="mc-form" class="group" name="casestudy_form" ng-submit="apiForm(user)">
+                                            <input type="text" class="form-control" name="phone" id="phnum" placeholder="Mobile Number" ng-model="user.phone" required="">
+                                            <span class="red-text" ng-show="casestudy_form.phone.$error.pattern">Please Enter a 10 Digit Number</span>
+                                            <button class="btn btn-theme" type="submit" ng-disabled="!user.phone || casestudy_form.$invalid"> Apply Now </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="secondary-footer" style="background-color: #000; position: relative; z-index: 10;">
+            <div class="container">
+                <div class="copyright">
+                    <div class="row align-items-center">
+                        <div class="col-md-12 sm-mt-2">
+                            <center><span>Copyright 2025 | All Rights Reserved</span> <span>Made in <i class="fa fa-heart" style="color: #FF0000;"></i> <a href="#">INDIA</a></span></center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- <a href="https://api.whatsapp.com/send?phone=917358518988&text=Hello%21%20Askeva" class="float" target="_blank" style="background-color: #0a6dc4; position: relative; z-index: 20;">
+            <img src="images/whatsappicon.png" alt="AskEVA" class="my-float">
+        </a> -->
+
+
+
+    <!--footer end-->
+    </body>
+
+</html>
+
---
+++ includes/header.php
@@ -0,0 +1,137 @@
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Optional: Add Owl Carousel CSS if not already included -->
+    <link rel="stylesheet" href="path-to-owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="path-to-owl-carousel/owl.theme.default.min.css">
+    <!-- Link to Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!-- Link to Font Awesome CDN in the <head> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
+<body>
+    <!-- navbar start -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" style="margin-left: 690px; color: aliceblue; font-size: 18px;">"Get Top Deal - <strong style="color: rgba(58, 145, 244, 0.5);">Unbeatable Prices.</strong>" Don't Miss Out - </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Book Now!
+                    </button>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                <button type="button" class="btn-close" aria-label="Close" style="color: aliceblue;"></button>
+            </span>
+            </div>
+        </div>
+    </nav>
+    <!-- model start -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Get your whatsapp API Now!</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <label>First Name*</label><br>
+                        <input type="text" name="FirstName*" value="" pattern="[A-Za-z]{8,}" required/><br><br>
+
+                        <label>Mobile No*</label><br>
+                        <input type="number" pattern="[A-Za-z1-9]{6}" required/><br><br>
+
+                        <label>Location*</label><br>
+                        <input type="text" name="FirstName*" value="" pattern="[A-Za-z]{8,}" required/><br><br>
+
+                        <label>Company Name*</label><br>
+                        <input type="text" name="FirstName*" value="" pattern="[A-Za-z]{8,}" required/><br><br>
+
+                        <div>
+                            <button type="submit" class="btn btn-primary" style="justify-content: center;display: flex;text-align: center;">Apply Now</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- model end -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" style="margin-left: 100px;"><img src="images/logo1.jpg" alt="" style="height: 80px; width: 290px;"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: 300px; font-size: 18px;">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #07b0ff;">
+                        Features
+                    </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li>
+                                <a class="dropdown-item" href="chatbot.html">Whatsapp API Chatbot</a>
+                            </li>
+                            <li><a class="dropdown-item" href="automation.html">Whatsapp Automation</a></li>
+                            <li><a class="dropdown-item" href="e-commerce.html">Whatsapp E-Commerce</a></li>
+                            <li><a class="dropdown-item" href="support.html">Whatsapp Customer Support</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Case studies
+                    </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="education.html">Education</a></li>
+                            <li><a class="dropdown-item" href="health.html">Health Care</a></li>
+                            <li><a class="dropdown-item" href="hr.html">HR</a></li>
+                            <li><a class="dropdown-item" href="automobile.html">Automobiles</a></li>
+                            <li><a class="dropdown-item" href="realestate.html">Real Estate</a></li>
+                            <li><a class="dropdown-item" href="travel.html">Travel & Hospitality</a></li>
+                            <li><a class="dropdown-item" href="insurance.html">Insurance</a></li>
+                            <li><a class="dropdown-item" href="ecommerce.html">E-Commerce</a></li>
+                            <li><a class="dropdown-item" href="retail.html">Retails</a></li>
+                        </ul>
+                    </li>
+                    <!-- <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Company
+                    </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="aboutus.html">About Us</a></li>
+                            <li><a class="dropdown-item" href="contactus.html">Contact Us</a></li>
+                        </ul> 
+                    </li> -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="aboutus.html">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contactus.html">Contact Us</a>
+                    </li>
+                </ul>
+                <button type="button" class="btn bg-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Login
+            </button>
+                <button type="button" class="btn " data-bs-toggle="modal" data-bs-target="#exampleModal" style="background-color: rgba(46, 181, 248, 0.5);">
+                Book Now!
+            </button>
+            </div>
+        </div>
+    </nav>
+    <!-- navbar end -->
